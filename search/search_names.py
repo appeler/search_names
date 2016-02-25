81f7eb6e307c34bf81c9862c5b897e2bb4338da6
Gaurav Sood <gsood07@gmail.com>
2016-02-25 16:04:33 -0500

    search names

---
+++ search/search_names.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import argparse
+import logging
+import csv
+try:
+    csv.field_size_limit(sys.maxsize)
+except:
+    csv.field_size_limit(sys.maxint)
+import time
+import signal
+
+from ConfigParser import ConfigParser
+from searchengines import (SearchMultipleKeywords, NewSearchMultipleKeywords,
+                           RESULT_FIELDS)
+
+from multiprocessing import (Pool, TimeoutError)
+from multiprocessing.managers import SyncManager
+from Queue import Empty
+
+import preprocess
+
+
+__version__ = "0.0.1"
+
+""" Defaults declaration
+"""
+MAX_NAME = 20
+LOG_FILE = 'search_names.log'
+DEF_OUTPUT_FILE = 'search_results.csv'
+DEFAULT_CONFIG_FILE = "search_names.cfg"
+NUM_PROCESSES = 4
+
+
+class WorkAroundManager(SyncManager):
+    @staticmethod
+    def _finalize_manager(process, address, authkey, state, _Client):
+        try:
+            SyncManager._finalize_manager(process, address, authkey, state,
+                                          _Client)
+        except WindowsError:
+            # http://stackoverflow.com/questions/17076679/windowserror-access-is-denied-on-calling-process-terminate
+            pass
+
+
+def setup_logger(debug):
+    """ Set up logging
+    """
+    level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(level=level,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filename=LOG_FILE,
+                        filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(level)
+    formatter = logging.Formatter('%(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+
+def parse_command_line():
+    """Parse command line arguments
+    """
+    parser = argparse.ArgumentParser(description="Search names in text corpus")
+
+    parser.add_argument('input', help='CSV input file name')
+    parser.add_argument("-c", "--config", type=str, dest="config",
+                        default=DEFAULT_CONFIG_FILE,
+                        help="Default configuration file\
+                        (default: {0!s})".format(DEFAULT_CONFIG_FILE))
+    parser.add_argument("-m", "--max-name", type=int, dest="max_name",
+                        default=MAX_NAME,
+                        help="Maximum name in search results (default: {0:d})"
+                        .format(MAX_NAME))
+    parser.add_argument("-p", "--processes", type=int, dest="processes",
+                        default=NUM_PROCESSES,
+                        help="Number of processes to run (default: {0:d})"
+                        .format(NUM_PROCESSES))
+    parser.add_argument("-o", "--out", type=str, dest="outfile",
+                        default=DEF_OUTPUT_FILE,
+                        help="Search results in CSV (default: {0:s})"
+                        .format(DEF_OUTPUT_FILE))
+
+    parser.add_argument('--overwritten', dest='overwritten',
+                        action='store_true',
+                        help='Overwritten if output file is exists')
+    parser.set_defaults(overwritten=False)
+
+    parser.add_argument('-d', '--debug', dest='debug',
+                        action='store_true',
+                        help="Enable debug message")
+    parser.set_defaults(debug=False)
+
+    parser.add_argument('--clean', dest='clean',
+                        action='store_true',
+                        help='Clean text column before search')
+    parser.set_defaults(clean=False)
+
+    return parser.parse_args()
+
+
+def load_config(args=None):
+    if args is None or isinstance(args, basestring):
+        namespace = argparse.Namespace()
+        if args is None:
+            namespace.config = DEFAULT_CONFIG_FILE
+        else:
+            namespace.config = args
+        args = namespace
+    try:
+        config = ConfigParser()
+        config.read(args.config)
+        args.namefile = config.get('name', 'file')
+        args.name_id = config.get('name', 'id')
+        args.name_search = config.get('name', 'search')
+        args.editlength = []
+        i = 1
+        while True:
+            k = 'edit' + str(i)
+            try:
+                l = config.getint('editlength', k)
+                args.editlength.append((l, i))
+                i += 1
+            except:
+                break
+    except Exception as e:
+        print(e)
+
+    return args
+
+
+def load_names_file(args):
+    args.names = []
+    logging.info("Load name file: {}".format(args.namefile))
+    with open(args.namefile, 'rb') as f:
+        reader = csv.DictReader(f)
+        for r in reader:
+            try:
+                args.names.append((r[args.name_id], r[args.name_search]))
+            except:
+                logging.error("Name file must have '{}' and '{}' columns"
+                              .format(args.name_id, args.name_search))
+                sys.exit(-2)
+    return args.names
+
+
+def clean_text(s):
+    s = preprocess.to_lower_case(s)
+    s = preprocess.remove_special_chars(s)
+    s = preprocess.remove_accents(s)
+    s = preprocess.remove_stopwords(s)
+    s = preprocess.remove_punctuation(s)
+    s = preprocess.remove_extra_space(s)
+    return s
+
+
+def init_worker():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+
+def worker(args):
+    try:
+        args, pid = args
+        logging.info('[{}] worker start'.format(pid))
+        namesearch = NewSearchMultipleKeywords(args.names, args.editlength)
+        with open(args.input, 'rb') as f:
+            reader = csv.DictReader(f)
+            count = 0
+            for i, r in enumerate(reader):
+                if (i % args.processes) != pid:
+                    continue
+                text = r['text']
+                if args.clean:
+                    # clean text if need
+                    text = clean_text(text)
+                start = time.time()
+                # Search and matching for nameslist
+                result, n = namesearch.search(text, args.max_name)
+                c = []
+                for k in reader.fieldnames:
+                    if args.clean and k == 'text':
+                        # replace original text with cleaned text
+                        c.append(text)
+                    else:
+                        c.append(r[k])
+                c.extend(result)
+                c.append(n)
+                elaspe = time.time() - start
+                logging.debug("[{}] found: {} in {:0.3f}s".format(pid, n, elaspe))
+                args.result_queue.put(c)
+                count += 1
+        logging.info('[{}] worker stop'.format(pid))
+        return count
+    except:
+        import traceback
+        traceback.print_exc()
+
+if __name__ == "__main__":
+
+    args = parse_command_line()
+
+    setup_logger(args.debug)
+
+    args = load_config(args)
+    logging.info(str(args))
+
+    logging.info("Setting up, please wait...")
+    load_names_file(args)
+
+    if args.clean:
+        preprocess.init_nltk()
+
+    new_outfile = True
+    """Create output file
+    """
+    try:
+        if not os.path.exists(args.outfile) or args.overwritten:
+            csvfile = open(args.outfile, 'wb')
+        else:
+            csvfile = open(args.outfile, 'ab')
+            new_outfile = False
+        csvwriter = csv.writer(csvfile, dialect='excel', delimiter=',',
+                               quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    except:
+        logging.error("Cannot create output file")
+        sys.exit(-1)
+
+    count = 0
+    all_start = time.time()
+
+    # Setting CSV header row
+    if new_outfile:
+        with open(args.input, 'rb') as f:
+            reader = csv.DictReader(f)
+            """Write output file headers
+            """
+            h = reader.fieldnames[:]
+            for i in range(args.max_name):
+                for a in RESULT_FIELDS:
+                    h.append('name%d.%s' % (i + 1, a))
+            h.append('count')
+            csvwriter.writerow(h)
+
+    # Setting up multiprocessing worker
+    manager = WorkAroundManager()
+    manager.start()
+    args.result_queue = manager.Queue()
+    pool = Pool(processes=args.processes, initializer=init_worker)
+    result = pool.map_async(worker,
+                            [(args, pid) for pid in range(args.processes)])
+
+    # Getting the result from worker processes though share queue
+    progress = 0
+    while True:
+        try:
+            r = args.result_queue.get(timeout=0.1)
+            csvwriter.writerow(r)
+            progress += 1
+            elaspe = time.time() - all_start
+            logging.info("Progress: {:d}, Average rate = {:.0f} rows/min"
+                          .format(progress, progress * 60 / elaspe))
+        except KeyboardInterrupt:
+            pool.terminate()
+            pool.join()
+            break
+        except Empty:
+            try:
+                done = result.get(timeout=0.1)
+                count = sum(done)
+                break
+            except TimeoutError:
+                pass
+
+    elaspe = time.time() - all_start
+    logging.info("Total: {:d}, Average rate = {:.0f} rows/min"
+                 .format(count, count * 60 / elaspe))
+    csvfile.close()
---
+++ search/searchengines.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Using `regex` instead of Python built-in `re` module.
+
+"""
+import regex as re
+from Levenshtein import distance
+
+"""Constant declaration
+"""
+MAX_RESULT = 20
+
+RESULT_FIELDS = ['uniqid', 'n', 'match', 'start', 'end']
+
+
+class SearchMultipleKeywords(object):
+    """Search by multiple keywords list
+    """
+    def __init__(self, keywords, fuzzy_min_len=[]):
+        """Initialize search
+        """
+        self.fuzzy_min_len = sorted(fuzzy_min_len)
+        self.keywords = {}
+        for i, k in keywords:
+            if i not in self.keywords:
+                self.keywords[i] = [k.strip().lower()]
+            else:
+                self.keywords[i].append(k.strip().lower())
+
+        print("Number of unique keywords ID to be search: {}"
+              .format(len(self.keywords)))
+
+        self.re_keywords = dict()
+        for i in self.keywords:
+            kw = []
+            for k in self.keywords[i]:
+                d = self.get_allow_distance(k)
+                if d:
+                    kw.append(r'(?:{0}){{e<={1}}}'.format(re.escape(k), d))
+                else:
+                    kw.append(re.escape(k))
+            re_str = '|'.join(kw)
+            re_str = r'\b(?:{})\b'.format(re_str)
+            self.re_keywords[i] = re.compile(re_str, flags=re.I)
+
+    def get_allow_distance(self, k):
+        dist = 0
+        for l, d in self.fuzzy_min_len:
+            if len(k) > l:
+                dist = d
+        return dist
+
+    def search(self, s, n=MAX_RESULT):
+        """Search
+        """
+        c = []
+        if n == 0:
+            return c
+
+        match = dict()
+        for k in self.re_keywords:
+            for a in self.re_keywords[k].finditer(s):
+                name = a.group(0).strip()
+                if k in match:
+                    match[k].append((name, a.start(0), a.end(0)))
+                else:
+                    match[k] = [(name, a.start(0), a.end(0))]
+
+        j = 0
+        for k in match:
+            c.extend([k, len(match[k]), ';'.join([m[0] for m in match[k]]),
+                      ';'.join([str(m[1]) for m in match[k]]),
+                      ';'.join([str(m[2]) for m in match[k]])])
+            j += 1
+            if j >= n:
+                break
+        while j < n:
+            for a in RESULT_FIELDS:
+                c.append('')
+            j += 1
+        count = 0
+        for m in match:
+            count += len(match[m])
+
+        return c, count
+
+
+class NewSearchMultipleKeywords(object):
+    """New search by multiple keywords (should be faster if large keywords)
+    """
+
+    def __init__(self, keywords, fuzzy_min_len=[]):
+        """Initialize search
+        """
+        self.fuzzy_min_len = sorted(fuzzy_min_len)
+        self.keywords = {}
+        for i, k in keywords:
+            k = k.strip().lower()
+            if k not in self.keywords:
+                self.keywords[k] = i
+            else:
+                print("ERROR: found duplicate keyword '{}'".format(k))
+
+        print("Number of unique keywords ID to be search: {}"
+              .format(len(self.keywords)))
+
+        kw = []
+        for k in self.keywords:
+            d = self.get_allow_distance(k)
+            if d:
+                kw.append(r'(?:{0}){{e<={1}}}'.format(re.escape(k), d))
+            else:
+                kw.append(re.escape(k))
+
+        re_str = '|'.join(kw)
+        re_str = r'\b(?:{})\b'.format(re_str)
+        self.re_keywords = re.compile(re_str)
+
+    def get_allow_distance(self, k):
+        dist = 0
+        for l, d in self.fuzzy_min_len:
+            if len(k) > l:
+                dist = d
+        return dist
+
+    def find_nearest_key(self, key):
+        key = key.encode('latin1')
+        for k in self.keywords:
+            d = self.get_allow_distance(k)
+            if distance(k, key) <= d:
+                return k
+
+    def search(self, s, n=MAX_RESULT):
+        """Search
+        """
+        c = []
+        if n == 0:
+            return c
+
+        j = 0
+        match = dict()
+        for a in self.re_keywords.finditer(s):
+            fkey = a.group(0).strip()
+            if fkey not in self.keywords:
+                fkey = a.group(0)
+                nearestkey = self.find_nearest_key(fkey)
+                #print("Approximate match '%s' ==> '%s'" % (fkey, nearestkey))
+                key = nearestkey
+            else:
+                key = fkey
+            rowid = self.keywords[key]
+            if rowid in match:
+                match[rowid].append((fkey, a.start(0), a.end(0)))
+            else:
+                match[rowid] = [(fkey, a.start(0), a.end(0))]
+
+        j = 0
+        for k in match:
+            c.extend([k, len(match[k]), ';'.join([m[0] for m in match[k]]),
+                      ';'.join([str(m[1]) for m in match[k]]),
+                      ';'.join([str(m[2]) for m in match[k]])])
+            j += 1
+            if j >= n:
+                break
+        while j < n:
+            for a in RESULT_FIELDS:
+                c.append('')
+            j += 1
+        count = 0
+        for m in match:
+            count += len(match[m])
+
+        return c, count
+
+
+if __name__ == "__main__":
+    import time
+    import csv
+
+    keywords = []
+    with open('../preprocess/deduped_augmented_clean_names.csv', 'rb') as f:
+        reader = csv.DictReader(f)
+        for r in reader:
+            keywords.append((r['uniqid'], r['search_name']))
+
+    n_result = 5
+    search_obj = SearchMultipleKeywords(keywords, [(10, 1), (15, 2)])
+    out = open('tests/output-1.csv', 'wb')
+    writer = csv.writer(out)
+    with open('tests/text_corpus.csv', 'rb') as f:
+        reader = csv.DictReader(f)
+        result_header = reader.fieldnames
+        for i in range(0, n_result):
+            for j in RESULT_FIELDS:
+                result_header.append('name{0}.{1}'.format(i + 1, j))
+        result_header.append('count')
+        writer.writerow(result_header)
+        start_time = time.time()
+        for i, r in enumerate(reader):
+            uid = r['uniqid']
+            text = r['text']
+            c = [uid, text]
+            result, count = search_obj.search(text, n_result)
+            c.extend(result)
+            c.append(count)
+            writer.writerow(c)
+        elaspe = time.time() - start_time
+        print("Average rate = %f rows/min" % (i * 60 / elaspe))
+    out.close()
+
+    # Test new search
+    n_result = 5
+    search_obj = NewSearchMultipleKeywords(keywords, [(10, 1), (15, 2)])
+    out = open('tests/output-2.csv', 'wb')
+    writer = csv.writer(out)
+    with open('tests/text_corpus.csv', 'rb') as f:
+        reader = csv.DictReader(f)
+        result_header = reader.fieldnames
+        for i in range(0, n_result):
+            for j in RESULT_FIELDS:
+                result_header.append('name{0}.{1}'.format(i + 1, j))
+        result_header.append('count')
+        writer.writerow(result_header)
+        start_time = time.time()
+        for i, r in enumerate(reader):
+            uid = r['uniqid']
+            text = r['text']
+            c = [uid, text]
+            result, count = search_obj.search(text, n_result)
+            c.extend(result)
+            c.append(count)
+            writer.writerow(c)
+        elaspe = time.time() - start_time
+        print("Average rate = %f rows/min" % (i * 60 / elaspe))
+    out.close()
