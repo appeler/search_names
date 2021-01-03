9d66d9aa1ccac67803dc85d15f88933c1189d626
***** <gsood07@gmail.com>
2021-01-02 23:36:35 -0800

    consolidate process_names

--- search-names/names.py
+++ search-names/names.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import argparse
 import re
 import csv
 from nameparser import HumanName
 
+DEFAULT_OUTPUT = "clean_names.csv"
 re_std_suffix = re.compile("(JR|SR|PHD)[^\.]", flags=re.I)
 
+def parse_command_line():
+    """Parse command line options
+    """
+    parser = argparse.ArgumentParser(description="Clean name")
 
-def process_name_list(infile, outfile=None, col="Name", all=False):
+    parser.add_argument('input', help='Input file name')
+
+    parser.add_argument("-o", "--out", action="store",
+                        type=str, dest="outfile", default=DEFAULT_OUTPUT,
+                        help="Output file in CSV (default: {0!s})"
+                        .format(DEFAULT_OUTPUT))
+    parser.add_argument("-c", "--column", action="store",
+                        type=str, dest="column", default="Name",
+                        help="Column name file in CSV contains Name list \
+                        (default: Name)")
+    parser.add_argument("-a", "--all", action="store_true",
+                        dest="all", default=False,
+                        help="Export all names (not take duplicate names out)\
+                        (default: False)")
+    return parser.parse_args()
+
+
+def process_names(infile, outfile=None, col="Name", all=False):
     """ Read names and pre-process
         Returns unique names in format "FirstName LastName AnyRomanNumeral"\
         or "FirstName LastName"
@@ -120,4 +145,15 @@ def process_name_list(infile, outfile=None, col="Name", all=False):
     return None
 
 if __name__ == '__main__':
-    pass
+    print("{0!s} - r4 (2016/01/02)\n".format(os.path.basename(sys.argv[0])))
+
+    args = parse_command_line()
+
+    print("Processing and exporting, please wait...")
+
+    """Process and export names file
+    """
+    process_names(args.input, args.outfile, args.column,
+                            args.all)
+
+    print("Done.")
