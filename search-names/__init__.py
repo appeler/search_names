ea7f4324409d59e66f69c30f84da88fe1d52293d
***** <gsood07@gmail.com>
2021-01-02 22:07:29 -0800

    add init

---
+++ search-names/__init__.py
@@ -0,0 +1,8 @@
+from .process_names import process_names
+from .preprocess import preprocess
+from .search_names import search_names
+from .merge_results import merge_results
+from .split_text_corpus import split_text_corpus
+from .merge_supp import merge_supp
+
+__all__ = ['process_names', 'preprocess', 'search_names', 'merge_results', 'merge_supp', 'split_text_corpus']
